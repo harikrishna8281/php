961e5d1ddc77a7e7e5d7c5878b97edf2773b9071
Harikrishna Sabu <hsabu@isimtl.local>
2019-11-16 16:38:42 -0500

    home

---
+++ Project/home.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html>
+<head>
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+<style>
+.card {
+  box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
+  max-width: 300px;
+  margin: auto;
+  text-align: center;
+  font-family: arial;
+  border-radius: 5px;
+}
+
+.title {
+  color: grey;
+  font-size: 18px;
+}
+
+img {
+  border-radius: 5px 5px 0 0;
+}
+button:hover, a:hover {
+  opacity: 0.7;
+}
+</style>
+</head>
+<body>
+
+<h2 style="text-align:center">User Profile Card</h2>
+
+<div class="card">
+  <img src="image/john.jpg" alt="John" style="width:100%">
+  <h1>John Doe</h1>
+  <p class="title">Single, 21</p>
+  <p>Avenue papineau</p>
+</div>
+
+</body>
+</html>
